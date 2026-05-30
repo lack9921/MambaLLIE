d6325d30e62d953a565f1d148f2ba483d4d745bc
lack9921 <linjack833@gmail.com>
2026-05-31 04:40:48 +0800

    更新__init__.py，添加calculate_lpips到可导出列表中

--- basicsr/metrics/__init__.py
+++ basicsr/metrics/__init__.py
@@ -2,17 +2,12 @@ from copy import deepcopy
 
 from basicsr.utils.registry import METRIC_REGISTRY
 from .psnr_ssim import calculate_psnr, calculate_ssim
+from .calculate_lpips import calculate_lpips
 
-__all__ = ['calculate_psnr', 'calculate_ssim']
+__all__ = ['calculate_psnr', 'calculate_ssim', 'calculate_lpips']
 
 
 def calculate_metric(data, opt):
-    """Calculate metric from data and options.
-
-    Args:
-        opt (dict): Configuration. It must contain:
-            type (str): Model type.
-    """
     opt = deepcopy(opt)
     metric_type = opt.pop('type')
     metric = METRIC_REGISTRY.get(metric_type)(**data, **opt)
